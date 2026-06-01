b84e2edb70a73cf5fc8de7585c91dc6eeaa91a98
streetreporter1 <m.compagno@streetreporter.net>
2026-06-01 12:11:34 +0200

    demo3
    bnbnmn

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/linkedin-engagement-insights/', // Inserisci qui il nome del tuo repository
   server: {
     port: 5173,
     host: true
